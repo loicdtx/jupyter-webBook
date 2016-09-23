7e875dcb5e63133ecbee0cbd136c99ff09d1c8d2
Loïc Dutrieux <loic.dutrieux@gmail.com>
2016-09-23 15:39:59 +0200

    Started working on dynamic builder

--- buid_site.py
+++ buid_site.py
@@ -2,37 +2,31 @@
 
 # Perhaps jinja2 is a more lightweigth solution
 from jinja2 import Template
+import json
+from os import makedirs
+from os.path import exists
+from pprint import pprint
 
-# Get the list of files (and title of the book) from the file "_book.yml"
+# Variables
+dir_out = "_site"
+
+# Get the list of files (and title of the book) from the file "_book.json"
+with open("_book.json") as src:
+    book_meta = json.load(src)
 
 # Create _site directory
+if not exists(dir_out):
+    makedirs(dir_out)
 
+pprint(book_meta)
 # For each file:
-	# Open and read it
-	# Generate html string
-	# Retrieve h1 and h2s (store the list of h1 and h2s for later)
-	# Generate file and add html string to it
+    # Open and read it
+    # Generate html string
+    # Retrieve h1 and h2s (store the list of h1 and h2s for later)
+    # Generate file and add html string to it
+
 
 # For each html file generated
-	# Read the file
-	# Fill the navbar
+    # Read the file
+    # Fill the navbar
 
-
-
-
-# Our template. Could just as easily be stored in a separate file
-template = """
-<html>
-<head>
-<title>Template {{ title }}</title>
-</head>
-<body>
-Body with {{ mystring }}.
-</body>
-</html>
-"""
-
-t = Template(template)
-c = Context({"title": "title from code",
-             "mystring":"string from code"})
-print t.render(c)
